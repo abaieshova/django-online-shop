1ae3854ab40bae2f6321fcb2672c5dde456d14a8
abaieshova <abaieshova@paragonisc.edu.kh>
2026-03-05 09:58:53 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run database migrations
+python manage.py migrate
+
+# Automatically create an admin user if it doesn't exist
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+
+# Start Gunicorn server
+gunicorn myshop.wsgi --log-file -

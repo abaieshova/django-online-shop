fd1eed0ad04636224e154ac461539e8bae04ea53
abaieshova <abaieshova@paragonisc.edu.kh>
2026-03-05 10:07:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,8 @@ python manage.py migrate
 # Automatically create an admin user if it doesn't exist
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 
+# Collect static files for WhiteNoise
+python manage.py collectstatic --noinput
+
 # Start Gunicorn server
 gunicorn myshop.wsgi --log-file -
